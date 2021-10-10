74349c999c45b17491f6e16f017bdd45641bab32
Kudaes <kurosh_2907@hotmail.com>
2021-10-10 14:08:53 +0200

    Update main.rs

--- dinvoke_rs/src/main.rs
+++ dinvoke_rs/src/main.rs
@@ -1,25 +1,6 @@
-//use bindings::Windows::Win32::Foundation::{HINSTANCE, PSTR};
-//use data::LoadLibraryA;
-//use std::ffi::CString;
 
 fn main() {
     println!("Hello, world!");
-    /*let a = manualmap::read_and_map_module("C:\\windows\\system32\\kernel32.dll").unwrap();
-    //let ret = dinvoke::call_module_entry_point(&a.0, a.1);
-
-    unsafe {
-        let function_ptr: LoadLibraryA; 
-        let name = CString::new("kernel32.dll".to_string()).expect("CString::new failed");
-        let function_name = PSTR{0: name.as_ptr() as *mut u8};
-        let mut abc: Option<HINSTANCE> = Option::None;
-        let ptr = dinvoke::get_module_base_address(&"kernel32.dll".to_string());
-        dinvoke::dynamic_invoke!(a.1,"LoadLibraryA",function_ptr,abc,function_name);
-
-        match abc {
-            Some(x) => println!("0x{:x}", x.0),
-            None => println!("Error"),
-        }
-    }*/
 }
 
 
