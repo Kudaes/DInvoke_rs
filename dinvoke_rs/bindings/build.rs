23705c6114a9e8a1ffe1385ae84f493827e44c7b
Kudaes <kurosh_2907@hotmail.com>
2023-04-23 21:36:32 +0200

    Fixes and funding

--- dinvoke_rs/bindings/build.rs
+++ dinvoke_rs/bindings/build.rs
@@ -5,7 +5,7 @@ fn main() {
             MINIDUMP_CALLBACK_INFORMATION},
         Windows::Win32::System::Memory::{VIRTUAL_ALLOCATION_TYPE,PAGE_PROTECTION_FLAGS},
         Windows::Win32::Foundation::{HANDLE,HINSTANCE,PSTR,BOOL},
-        Windows::Win32::System::Threading::{GetCurrentProcess,GetCurrentThread,PROCESS_BASIC_INFORMATION},
+        Windows::Win32::System::Threading::{GetCurrentProcess,GetCurrentThread,ExitProcess,PROCESS_BASIC_INFORMATION},
         Windows::Win32::System::SystemServices::{IMAGE_BASE_RELOCATION,IMAGE_IMPORT_DESCRIPTOR,IMAGE_THUNK_DATA32,IMAGE_THUNK_DATA64,OVERLAPPED},
         Windows::Win32::System::WindowsProgramming::{PUBLIC_OBJECT_TYPE_INFORMATION,OBJECT_ATTRIBUTES,IO_STATUS_BLOCK},
         Windows::Win32::Security::SECURITY_ATTRIBUTES,
